8ea0aa1081716be9d66311eb9f6f138275e5e207
Michael Ambrus <michael.ambrus@medius.se>
2017-03-14 22:05:31 +0100

    Vector: First implementation
    With abilities:
    
    * Generic (a.k.a. "template")
    * Indexer (a.k.a. "index operator")

--- polygons_cs/Program.cs
+++ polygons_cs/Program.cs
@@ -10,6 +10,69 @@ namespace polygons_cs
   {
     static void Main(string[] args)
     {
+
+      Vector<int> myVector = new Vector<int>(10);
+
+      myVector[0] = 1;
+      myVector[1] = 2;
+      myVector[2] = 3;
+      myVector[3] = 4;
+      myVector[4] = 5;
+      myVector[5] = 6;
+      myVector[6] = 7;
+      myVector[7] = 8;
+      myVector[8] = 9;
+      myVector[9] = 10;
+
+      //using the first indexer with int parameter to print int Vector
+      for (int i = 0; i < myVector.size(); i++)
+      {
+        Console.WriteLine(i + ": " + myVector[i]);
+      }
+
+      Vector<double> myVector2 = new Vector<double>(10);
+
+      myVector2[0] = 1.1;
+      myVector2[1] = 2.2;
+      myVector2[2] = 3.3;
+      myVector2[3] = 4.4;
+      myVector2[4] = 5.5;
+      myVector2[5] = 6.6;
+      myVector2[6] = 7.7;
+      myVector2[7] = 8.8;
+      myVector2[8] = 9.9;
+      myVector2[9] = 10.0;
+
+      //using the first indexer with int parameter to print float Vector
+      for (int i = 0; i < myVector2.size(); i++)
+      {
+        Console.WriteLine(i + ": " + myVector2[i]);
+      }
+
+      Vector<Vector<double>> myVector3 = new Vector<Vector<double>>(10);
+
+      myVector3[0] = myVector2;
+      myVector3[1] = myVector2;
+      myVector3[2] = myVector2;
+      myVector3[3] = myVector2;
+      myVector3[4] = myVector2;
+      myVector3[5] = myVector2;
+      myVector3[6] = myVector2;
+      myVector3[7] = myVector2;
+      myVector3[8] = myVector2;
+      myVector3[9] = myVector2;
+
+      //using the first indexer with int parameter to print float Vector
+      for (int i = 0; i < myVector3.size(); i++)
+      {
+        for (int j = 0; j < myVector2.size(); j++)
+        {
+          Console.WriteLine("[" + i + "][" + j + "]: " + myVector3[i][j]);
+        }
+      }
+
+      Console.ReadKey();
     }
   }
 }
+
--- polygons_cs/Vector.cs
+++ polygons_cs/Vector.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace polygons_cs
 {
-  class Vector
+  class Vector<VTYPE>
   {
+    private VTYPE[] m_v;
+
+    public Vector(int size)
+    {
+      m_v = new VTYPE[size];
+    }
+
+    public int size()
+    {
+      return m_v.Length;
+    }
+
+    public VTYPE this[int index]
+    {
+      get
+      {
+
+        if (index >= 0 && index < m_v.Length)
+        {
+          return m_v[index];
+        }
+        else
+        {
+          //Put trow here
+          return m_v[0];
+        }
+      }
+      set
+      {
+        if (index >= 0 && index < m_v.Length)
+        {
+          m_v[index] = value;
+        }
+        else
+        {
+          //Put trow here
+        }
+      }
+ 
+    }
   }
 }
+
